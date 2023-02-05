eac9e64d03d0be6854d7004b08145e272683df52
Lokesh Choudhary <lokeshchoudhary2026@gmail.com>
2023-02-05 17:33:08 +0530

    Done Project

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechyStar</h1>
+        <p> 	&reg; all right reserved </p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+        
+          <a href="https://www.instagram.com/lokeshchoudharyprogrammer/" target={"_blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/lokeshchoudharyprogrammer/" target={"_blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
